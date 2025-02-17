3126e8db586ba252c5f88540dc5eff207b067d08
Mohammad Rajaei <m.rajaei@visiwise.co>
2025-02-17 18:35:54 +0330

    fix: fix rollup configs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,47 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import path from "path";
 import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import dts from "rollup-plugin-dts";
+import { createRequire } from "module";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: "dist/index.esm.js",
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  plugins: [resolve(), commonjs(), typescript()],
-};
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.cjs",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.mjs",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+      "playwright",
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: path.resolve("tsconfig.json"),
+        declaration: true,
+        declarationDir: "dist/types",
+        rootDir: "src",
+      }),
+    ],
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
